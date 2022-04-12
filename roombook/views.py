47746a1ddfff983927cc15912ba8821af3f406bb
bobshort4bobby4 <treborken@gmail.com>
2022-04-12 16:33:42 +0000

    install allauth

--- roombook/views.py
+++ roombook/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
+from django.shortcuts import render, redirect, reverse
+from django.views.generic import ListView ,FormView, View
 # Create your views here.
+
+
+# class Home(ListView):
+#     # model = RoomType
+#     template_name = 'home.html'
+    
